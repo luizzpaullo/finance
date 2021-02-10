44c174d9ab87dda44d7263164285c59c34412851
LuizzP <luizz.paullo@gmail.com>
2021-02-09 22:44:36 -0400

    Refaturando0

---
+++ scripts.js
@@ -0,0 +1,177 @@
+const Modal = {
+    open() {
+        //abrir modal
+        //adicionar class active ao modal
+        document
+            .querySelector('.modal-overlay')
+            .classList
+            .add('active')
+
+    },
+    close() {
+        //fechar modal
+        //remover a class active do modal
+        document
+            .querySelector('.modal-overlay')
+            .classList
+            .remove('active')
+
+    }
+}
+
+
+const transactions = [
+    {
+        id: 1,
+        description: 'Energia',
+        amount: -500010,
+        date: '23/01/2021',
+    },
+    {
+        id: 2,
+        description: 'Site',
+        amount: 3500020,
+        date: '23/01/2021',
+    },
+    {
+        id: 3,
+        description: 'Internet',
+        amount: -200300,
+        date: '23/01/2021',
+    },
+    {
+        id: 4,
+        description: 'App',
+        amount: 2500040,
+        date: '09/02/2021',
+    },
+]
+
+const Transaction = {
+    all: transactions,
+    
+    incomes() {
+           //somar as entradas
+           let income = 0;
+           //Pegar todas as transactions
+           //para cada transaction
+           transactions.forEach(transaction => {
+               //se ela forma maior que zero
+               if (transaction.amount > 0) {
+                   //somar a uma variável e retornar a variável a
+                   // income = income + transaction.amount;
+                    income += transaction.amount;
+
+               }
+           })
+        return income; //"Entrada"
+     
+
+
+    },
+    expenses() {
+        //somar as saídas
+        let expense = 0;
+        //Pegar todas as transactions
+        //para cada transaction
+        transactions.forEach(transaction => {
+            //se ela forma MENOR que zero
+            if (transaction.amount < 0) {
+                //somar a uma variável e retornar a variável a
+                // income = income + transaction.amount;
+                 expense += transaction.amount;
+
+            }
+        })
+     return expense; //"Entrada"
+
+
+    },
+    total() {
+        //total = entradas - saídas
+        return Transaction.incomes() + Transaction.expenses()
+    }
+
+}
+
+const DOM ={
+    transactionsContainer: document.querySelector('#data-table tbody'),
+
+
+    addTransaction(transaction, index) {
+        // console.log(transaction)
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+       // console.log(tr.innerHTML) // testando no console do navegador 
+
+
+       DOM.transactionsContainer.appendChild(tr)
+    },
+    innerHTMLTransaction(transaction) {
+        //colocando a classe correta Entrada || Saida
+        const CSSClass = transaction.amount > 0 ? "income" : "expense"
+
+        //formato de Moeda
+        const amount = Utils.formatCurrency(transaction.amount)
+        const html = `
+        
+                   
+                            <td class="description">${transaction.description}</td>
+                            <td class="${CSSClass}">${amount}</td>
+                            <td class="date">${transaction.date}</td>
+                            <td>
+                                 <img src="assets/minus.svg" alt="Botão de Remover lançamento" />
+                            </td>
+                    
+        
+        `
+        return html
+    },
+    updateBalance(){
+        document
+            .getElementById('incomeDisplay')
+            .innerHTML = Utils.formatCurrency(Transaction.incomes())
+        document
+            .getElementById('expenseDisplay')
+            .innerHTML = Utils.formatCurrency(Transaction.expenses() )//"Soma das Entradas"
+        document
+            .getElementById('totalDisplay')
+            .innerHTML =Utils.formatCurrency(Transaction.total()) // "Soma das Entradas"
+    },
+    
+}
+
+const Utils = {
+    formatCurrency(value) {
+        // console.log(value)
+
+
+        const signal = Number(value) < 0 ? "-" : "";
+        // console.log(signal)
+
+        // convertendo para string para ativar REGEX
+        value = String(value).replace(/\D/g,"")
+
+        value = Number(value)/ 100
+
+        value = value.toLocaleString("pt-BR",{
+            style: "currency",
+            currency: "BRL"
+        })
+
+        // console.log(value)
+        return signal + value
+    }
+}
+
+
+// DOM.addTransaction(transactions[0])
+// DOM.addTransaction(transactions[1])
+// DOM.addTransaction(transactions[2])
+
+transactions.forEach(function (transaction) {
+    DOM.addTransaction(transaction)
+})
+
+DOM.updateBalance()
