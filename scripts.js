ca99e17bbed73ed7ac05494c2fde35b2a1cf8495
LuizzP <luizz.paullo@gmail.com>
2021-02-12 07:25:14 -0400

    error na validacao do form

--- scripts.js
+++ scripts.js
@@ -164,6 +164,19 @@ const DOM ={
 }
 
 const Utils = {
+    formatAmount(value) {
+        value = Number(value.replace(/\,\./g,"")) * 100
+        // console.log(value)
+
+        return value
+    },
+
+    formatDate(date) {
+        const splittedDate = date.split("-")
+        // console.log(splittedDate)
+        return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`
+    },
+    
     formatCurrency(value) {
         // console.log(value)
 
@@ -183,11 +196,76 @@ const Utils = {
 
         // console.log(value)
         return signal + value
-    }
+    },
 }
 
 const Form ={
+    description: document.querySelector('input#description'),
+    amount: document.getElementById('#amount'),
+    date: document.getElementById('#date'),
 
+    getValues(){
+        return {
+            description:Form.description.value,
+            amount:Form.amount.value,
+            date:Form.date.value
+        }
+    },
+
+    validateFields(){
+        //console.log('validar campos')
+        const { description, amount, date } = Form.getValues()
+
+        if( description.trim() === "" ||
+            amount.trim() === "" || 
+            date.trim() === "" ) {
+                throw new Error("Por favor, preencha todos os campos")
+        }
+    },
+    
+    formatValues(){
+        let {description, amount, date} = Form.getValues()
+
+            amount = Utils.formatAmount(amount)
+
+            date = Utils.formatDate(date)
+
+        
+
+
+            return {
+                description, 
+                amount,
+                 date
+                }
+
+    },
+
+    clearFields(){
+        Form.description.value ="";
+        Form.amount.value ="";
+        Form.date.value ="";
+
+    },
+
+    submit(event) {
+        event.preventDefault() 
+
+        try {
+            Form.validateFields() 
+            const transaction = Form.formatValues()
+            Transaction.add(transaction)
+            Form.clearFields()
+            Modal.close()
+    
+            
+        } catch (error) {
+            alert(error.message)
+        }
+
+       
+
+    }
 }
 
 const App = {
